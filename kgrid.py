9b1ffbd3326a7418e2c0e4e7c06497ec966ba6d9
Adam J. Jackson <a.j.jackson@physics.org>
2016-07-28 14:51:34 +0100

    Reformat to PEP8 compliance

--- kgrid.py
+++ kgrid.py
@@ -2,9 +2,9 @@
 
 """Get k-grid parameters for desired length cutoff and input geometry"""
 
-##############################################################################################
+###############################################################################
 # Copyright 2015 Adam Jackson
-##############################################################################################
+###############################################################################
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -17,13 +17,15 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################################
+###############################################################################
 
 import numpy as np
 from optparse import OptionParser
 import ase.io
 
-def calc_grid(cutoff_length, filename='geometry.in', filetype=False, pretty_print=False):
+
+def calc_grid(cutoff_length, filename='geometry.in',
+              filetype=False, pretty_print=False):
 
     if filetype:
         atoms = ase.io.read(filename, format=filetype)
@@ -38,33 +40,35 @@ def calc_grid(cutoff_length, filename='geometry.in', filetype=False, pretty_prin
     else:
         return k_samples
 
+
 def calc_kpt_tuple(atoms, cutoff_length=10):
-    """Calculate Moreno-Soler k-point grid for ASE atoms object and given real-space cutoff"""
+    """Calculate Moreno-Soler k-point grid for ASE atoms object"""
 
     # Import lattice vectors using ASE
     lattice_vectors = atoms.cell
 
     # Get lattice vector magnitudes (a, b, c) according to Pythagoras' theorem
-    abc = np.sqrt(np.sum(np.square(lattice_vectors),1))
+    abc = np.sqrt(np.sum(np.square(lattice_vectors), 1))
 
     # k-point samples required = 2*cutoff_length/(magnitude of lattice vector)
-    k_samples = np.divide(2*cutoff_length,abc)
+    k_samples = np.divide(2*cutoff_length, abc)
     # Round up
     k_samples = np.ceil(k_samples)
     return tuple(k_samples)
 
-
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option("-c", "--cutoff-length",
-                      action="store", type="float", dest="cutoff_length", default=10.0,
+    parser.add_option("-c", "--cutoff-length", action="store", type="float",
+                      dest="cutoff_length", default=10.0,
                       help="Set length cutoff in Angstroms [default: 10]")
-    parser.add_option("-f", "--file",
-                      action="store", type="string", dest="file", default="geometry.in",
+    parser.add_option("-f", "--file", action="store", type="string",
+                      dest="file", default="geometry.in",
                       help="Path to input file [default: ./geometry.in]")
-    parser.add_option("-t", "--type", action="store", type="string", default=False,
+    parser.add_option("-t", "--type", action="store", type="string",
+                      default=False,
                       help="Input file type. If not provided, ASE will guess.")
     # Add further options here
     (options, args) = parser.parse_args()
 
-    calc_grid(options.cutoff_length, filename=options.file, filetype=options.type, pretty_print=True)
+    calc_grid(options.cutoff_length, filename=options.file,
+              filetype=options.type, pretty_print=True)
