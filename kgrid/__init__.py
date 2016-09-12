8709119ca06f80ace642eba414018f4dd97aae7c
Adam J. Jackson <a.j.jackson@physics.org>
2016-09-12 21:46:34 +0100

    restructure for packaging; setup.y

--- kgrid/__init__.py
+++ kgrid/__init__.py
@@ -1,9 +1,5 @@
-#! /usr/bin/env python
-
-"""Get k-grid parameters for desired length cutoff and input geometry"""
-
 ###############################################################################
-# Copyright 2015 Adam Jackson
+# Copyright 2016 Adam Jackson
 ###############################################################################
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -20,27 +16,6 @@
 ###############################################################################
 
 import numpy as np
-from argparse import ArgumentParser
-import ase.io
-
-def calc_grid(cutoff_length, mode='default', filename='geometry.in',
-              filetype=False, realspace=False, pretty_print=False):
-
-    if filetype:
-        atoms = ase.io.read(filename, format=filetype)
-    else:
-        atoms = ase.io.read(filename)
-
-    k_samples = calc_kpt_tuple(atoms, mode=mode,
-                               cutoff_length=cutoff_length,
-                               realspace=realspace)
-
-    # Print vectors
-    if pretty_print:
-        print '{0:3.0f} {1:3.0f} {2:3.0f}'.format(*k_samples)
-    else:
-        return k_samples
-
 
 def calc_kpt_tuple(atoms, cutoff_length=10, realspace=False, mode='default'):
     """Calculate k-point mesh from ASE atoms object for real-space cutoff
@@ -124,43 +99,3 @@ def calc_kpt_tuple_recip(atoms, cutoff_length=10, rounding='up'):
         k_samples = np.floor(k_samples + 0.5)
     return tuple((int(x) for x in k_samples))
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    threshold = parser.add_mutually_exclusive_group()
-    threshold.add_argument("-c", "--cutoff-length",
-                      action="store", type=float, dest="cutoff_length",
-                      default=10.0,
-                      help="Set length cutoff in Angstroms [default: 10]")
-    threshold.add_argument("-a", "--vasp-auto", action="store", type=float,
-                      dest="vasp_auto",
-                      help="Real-space cutoff like Auto in VASP KPOINTS file")
-    threshold.add_argument("-s", "--vasp-kspacing",
-                      action="store", type=float, dest="kspacing",
-                      help="Reciprocal space distance like KSPACING in VASP")
-
-    parser.add_argument("-f", "--file",
-                      action="store", type=str, dest="file",
-                      default="geometry.in",
-                      help="Path to input file [default: ./geometry.in]")
-    parser.add_argument("-t", "--type", action="store", type=str,
-                      default=False,
-                      help="Input file type. If not provided, ASE will guess.")
-    parser.add_argument("-r", "--realspace", action="store_true",
-                      help="Use real-space vector lengths instead of "
-                           "computing reciprocal cell; not recommended!")
-    # Add further options here
-    args = parser.parse_args()
-
-    if args.vasp_auto:
-        mode = 'vasp_auto'
-        cutoff = args.vasp_auto
-    elif args.kspacing:
-        mode = 'kspacing'
-        cutoff = args.kspacing
-    else:
-        mode = 'default'
-        cutoff = args.cutoff_length
-
-    calc_grid(cutoff, mode=mode, filename=args.file,
-              filetype=args.type, realspace=args.realspace,
-              pretty_print=True)
