5486e981c623502bc0dedba80ae38c0a4356e032
Adam J. Jackson <a.j.jackson@physics.org>
2019-10-25 11:09:27 +0100

    update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -9,28 +9,31 @@ project_dir = abspath(dirname(__file__))
 
 setup(
     name='kgrid',
-    version='1.0.0',
-    description='Reciprocal space sampling for crystal structures',
+    version='1.1.0',
+    description='Reciprocal space sampling for atomistic crystal structures',
     long_description="""
 Generate reciprocal-space grids with scalar cutoff parameters and standard
 crystal structure files. kgrid helps with k-point convergence problems when 
 using ab initio codes that lack a single-parameter option, and help you
 understand and plan calculations with codes that do.
 """,
-    url="https://github.com/WMD-group/kgrid",
-    author="Adam J. Jackson / Walsh Materials Design group",
-    author_email="a.j.jackson@physics.org",
+    long_description_content_type='text/markdown',
+    url='https://github.com/WMD-group/kgrid',
+    author='Adam J. Jackson',
+    author_email='a.j.jackson@physics.org',
     license='GPL v3',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Physics'
         ],
+    python_requires=('>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, '
+                     '!=3.5.*, >=3.6, <4'),
     keywords='chemistry physics k-point sampling reciprocal',
     packages=find_packages(exclude=['test']),
     install_requires=['ase'],
